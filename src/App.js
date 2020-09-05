b0580c9d13e418219ae7615a449f16f7cc61b958
unknown <ismailajizoumo@gmail.com>
2020-09-05 12:11:19 +0100

    setting up the shop page with the collections preview

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
 import { Route } from 'react-router-dom';
+import ShopPage from './pages/shop/Shop';
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
