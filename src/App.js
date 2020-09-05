1f3e5615e364900a387fd6446a4699b43da0cc55
unknown <ismailajizoumo@gmail.com>
2020-09-05 10:41:18 +0100

    adding the routing functionnalities

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage}/>
     </div>
   );
 }
--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -46,8 +46,8 @@ class Directory extends React.Component {
         return ( 
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem title={title} imageUrl={imageUrl} key={id} size={size}/>
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                        <MenuItem key={id} {...otherSectionProps}/>
                     ))
                 }
             </div>
