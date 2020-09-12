cf117a26fc415faf0ab106003c27170e713068b7
unknown <ismailajizoumo@gmail.com>
2020-09-12 19:54:38 +0100

    moving the sections to a specific reducer

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item';
-import SECTIONS from './sections.data';
 import './directory.scss';
+import { connect } from 'react-redux';
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+import {createStructuredSelector} from 'reselect'
 
-class Directory extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            sections: SECTIONS
-         }
-    }
-    render() { 
-        return ( 
+const Directory =({ sections }) => ( 
             <div className='directory-menu'>
-                {
-                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                {sections.map(({id, ...otherSectionProps}) => (
                         <MenuItem key={id} {...otherSectionProps}/>
-                    ))
-                }
+                    ))}
             </div>
          );
-    }
-}
  
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections
+})
+
+export default  connect(mapStateToProps)(Directory);
