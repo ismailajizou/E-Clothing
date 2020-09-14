880c3f27b9f43bd30cd71f7e2835248db807a16f
unknown <ismailajizoumo@gmail.com>
2020-09-14 23:01:54 +0100

    normalizing shop data

---
+++ src/components/collections-overview/collections-overview.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
+import CollectionPreview from '../collection-preview/collection-preview';
+import './collections-overview.scss';
+
+
+const CollectionsOverview = ({collections}) => ( 
+    <div className='collections-overview'>
+        {collections.map(({id, ...otherProps})=> (
+            <CollectionPreview key={id} {...otherProps} />
+        ))}
+    </div>
+ );
+
+ const mapStateToProps = createStructuredSelector({
+    collections: selectCollectionsForPreview
+})
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,29 +1,16 @@
-import React, { Component } from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import React from 'react';
+import CollectionsOverview from '../../components/collections-overview/collections-overview';
+import {Route} from 'react-router-dom';
+import CollectionPage from '../collection/collection';
 
-
-
-class ShopPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            collections: SHOP_DATA
-         }
-    }
-    
-    render() { 
-        const { collections } = this.state;
-        return ( 
-            <div className='shop-page'>
-                {
-                    collections.map(({id, ...otherProps})=> (
-                        <CollectionPreview key={id} {...otherProps} />
-                    ))
-                }
-            </div>
-         );
-    }
+const ShopPage = ({ match }) => {
+    return( 
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`} component={CollectionsOverview} />
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+        </div>
+    );
 }
- 
+
+
 export default ShopPage;
