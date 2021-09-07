7b8f2b5ca0b53f5052563af0eb6ba0075e7f73e3
unknown <ismailajizoumo@gmail.com>
2021-09-07 13:55:01 +0100

    new push

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY.,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "e-clothing-db-2e653.firebaseapp.com",
-    databaseURL:process.env.REACT_APP_FIREBASE_DB_URL.,
+    databaseURL:process.env.REACT_APP_FIREBASE_DB_URL,
     projectId: "e-clothing-db-2e653",
     storageBucket: "e-clothing-db-2e653.appspot.com",
     messagingSenderId: "23529814391",
-    appId: process.env.REACT_APP_FIREBASE_APP_ID.,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: "G-MHFYBVCZ7Z"
 };
 
