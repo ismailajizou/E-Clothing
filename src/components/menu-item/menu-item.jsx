b74196b2e97ea3ca6b75ab088a342e75c456d409
Ismail Ajizou <ismailajizoumo@gmail.com>
2022-09-04 17:58:16 +0100

    fixed link issue

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "./menu-item.scss";
 
-import './menu-item.scss';
-import { useHistory } from 'react-router-dom';
-
-const MenuItem = ({title, imageUrl, size, linkUrl,match}) => {
-    const history = useHistory();
-    return ( 
-        <div className={`${size ? size : ''} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
-        <div className='background-image' style={{
-            backgroundImage: `url(${imageUrl})`
-        }}/>
-        <div className='content'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'>SHOP NOW</span>
-        </div>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const history = useHistory();
+  return (
+    <div
+      className={`${size ? size : ""} menu-item`}
+      onClick={() => history.push(linkUrl)}
+    >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-     );
-}
- 
+  );
+};
+
 export default MenuItem;
